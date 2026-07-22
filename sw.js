ad8ece5cd4a497dbcac6b7834d7bfbb7d2b7e980
thesmrtalec <alec.g.stubbs@gmail.com>
2026-07-22 16:10:10 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
  * Bump CACHE on each release. Nothing else needs touching.
  */
 
-var CACHE = "jtb-v1.2.4";
+var CACHE = "jtb-v1.2.5";
 
 self.addEventListener("install", function (e) {
   // don't sit in "waiting" behind the old worker
